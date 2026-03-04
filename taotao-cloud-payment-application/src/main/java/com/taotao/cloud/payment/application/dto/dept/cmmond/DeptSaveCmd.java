e67bc64812c971e7069bbcca07875726a48b4a56
shuigedeng <981376577@qq.com>
2026-03-04 16:38:25 +0800

    开启2026.03

--- taotao-cloud-payment-application/src/main/java/com/taotao/cloud/payment/application/dto/dept/cmmond/DeptSaveCmd.java
+++ taotao-cloud-payment-application/src/main/java/com/taotao/cloud/payment/application/dto/dept/cmmond/DeptSaveCmd.java
@@ -41,7 +41,7 @@ public class DeptSaveCmd implements Serializable {
 
     @Serial private static final long serialVersionUID = -4132785717179910025L;
 
-    @Schema(description = "部门id", requiredMode = Schema.RequiredMode.REQUIRED)
+    @Schema(description = "部门id")
     private Integer deptId;
 
     @Schema(description = "部门名称")
--- taotao-cloud-payment-application/src/main/java/com/taotao/cloud/payment/application/dto/dict/query/DictPageQry.java
+++ taotao-cloud-payment-application/src/main/java/com/taotao/cloud/payment/application/dto/dict/query/DictPageQry.java
@@ -16,7 +16,7 @@
 
 package com.taotao.cloud.payment.application.dto.dict.query;
 
-import com.taotao.boot.common.model.request.PageQuery;
+import com.taotao.boot.common.model.ddd.query.PageQuery;
 import lombok.*;
 import lombok.experimental.Accessors;
 
